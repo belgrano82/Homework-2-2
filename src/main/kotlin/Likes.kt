96347c65c8a9e6b28b717cce9d99671f9baa4742
Konstantin Kokov <superstarforit@gmail.com>
2023-03-24 04:25:00 +0300

    add interface attachments

--- src/main/kotlin/Likes.kt
+++
@@ -1,1 +0,0 @@
-data class Likes(val count: Int, val userLikes: Int, val canLike: Boolean, val canPublish: Boolean)
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -15,58 +15,274 @@ class WallServiceTest {
     @Test
     fun addNewPostTest() {
 
-        val testPost = Post(0, 111, 222, "New post",
+        val testPost = Post(
+            0,
+            111,
+            222,
+            111,
+            1679447042,
+            "New post",
+            333,
+            444,
+            true,
+            null,
+            Copyright(5, "www", "String", "String"),
             Likes(0, 0, true, true),
-            Comments(0, canPost = true, groupsCanPost = true, canClose = true, canOpen = true))
+            Reposts(0, false),
+            Views(0),
+            "string",
+            postSource = null,
+            null,
+            null,
+            copyHistory = ArrayList<Reposts>(),
+            null,
+            null,
+            null,
+            true,
+            true,
+            true,
+            Donut(true, 10, placeHolder = null, true, "string"),
+            100,
+            attachments = null
+
+        )
+
 
         val result = add(post = testPost)
 
 
-        assertEquals( Post(1, 111, 222, "New post",
-            Likes(0, 0, true, true),
-            Comments(0, true, true, true, true)), result)
+        assertEquals(
+            Post(1,
+                111,
+                222,
+                111,
+                1679447042,
+                "New post",
+                333,
+                444,
+                true,
+                null,
+                Copyright(5, "www", "String", "String"),
+                Likes(0, 0, true, true),
+                Reposts(0, false),
+                Views(0),
+                "string",
+                postSource = null,
+                null,
+                null,
+                copyHistory = ArrayList<Reposts>(),
+                null,
+                null,
+                null,
+                true,
+                true,
+                true,
+                Donut(true, 10, placeHolder = null, true, "string"),
+                100,
+                attachments = null
+            ), result
+        )
     }
 
     @Test
     fun updateIfTrue() {
-//        val posts = arrayOf(Post(5, 111, 222, "New post1",
-//            Likes(0, 0, true, true),
-//            Comments(0, canPost = true, groupsCanPost = true, canClose = true, canOpen = true)), Post(10, 112, 223, "New post2",
-//            Likes(0, 0, true, true),
-//            Comments(0, canPost = true, groupsCanPost = true, canClose = true, canOpen = true)))
-        val testPost1 = Post(0, 111, 222, "New post1",
+
+        val testPost1 = Post(
+            0,
+            111,
+            222,
+            111,
+            1679447042,
+            "New post1",
+            333,
+            444,
+            true,
+            null,
+            Copyright(5, "www", "String", "String"),
             Likes(0, 0, true, true),
-            Comments(0, canPost = true, groupsCanPost = true, canClose = true, canOpen = true))
-        val testPost2 = Post(0, 112, 223, "New post2",
+            Reposts(0, false),
+            Views(0),
+            "string",
+            postSource = null,
+            null,
+            null,
+            copyHistory = ArrayList<Reposts>(),
+            null,
+            null,
+            null,
+            true,
+            true,
+            true,
+            Donut(true, 10, placeHolder = null, true, "string"),
+            100,
+            attachments = null
+        )
+        val testPost2 = Post(
+            0,
+            111,
+            222,
+            111,
+            1679447042,
+            "New post2",
+            333,
+            444,
+            true,
+            null,
+            Copyright(5, "www", "String", "String"),
             Likes(0, 0, true, true),
-            Comments(0, canPost = true, groupsCanPost = true, canClose = true, canOpen = true))
+            Reposts(0, false),
+            Views(0),
+            "string",
+            postSource = null,
+            null,
+            null,
+            copyHistory = ArrayList<Reposts>(),
+            null,
+            null,
+            null,
+            true,
+            true,
+            true,
+            Donut(true, 10, placeHolder = null, true, "string"),
+            100,
+            attachments = null
+        )
         add(post = testPost1)
         add(post = testPost2)
-        val testPostUpdated = Post(2, 111_000, 222_000, "New post Updated",
+        val testPostUpdated = Post(
+            2,
+            111,
+            222,
+            111,
+            1679447042,
+            "New post updated",
+            333,
+            444,
+            true,
+            null,
+            Copyright(5, "www", "String", "String"),
             Likes(0, 0, true, true),
-            Comments(0, canPost = true, groupsCanPost = true, canClose = true, canOpen = true))
+            Reposts(0, false),
+            Views(0),
+            "string",
+            postSource = null,
+            null,
+            null,
+            copyHistory = ArrayList<Reposts>(),
+            null,
+            null,
+            null,
+            true,
+            true,
+            true,
+            Donut(true, 10, placeHolder = null, true, "string"),
+            100,
+            attachments = null
+        )
         val result = update(post = testPostUpdated)
 
-        assertEquals(true, result)
+
+        assertTrue("true", result)
 
     }
 
     @Test
-    fun updateIfFalse(){
-        val testPost1 = Post(0, 111, 222, "New post1",
+    fun updateIfFalse() {
+        val testPost1 = Post(
+            0,
+            111,
+            222,
+            111,
+            1679447042,
+            "New post1",
+            333,
+            444,
+            true,
+            null,
+            Copyright(5, "www", "String", "String"),
             Likes(0, 0, true, true),
-            Comments(0, canPost = true, groupsCanPost = true, canClose = true, canOpen = true))
-        val testPost2 = Post(0, 112, 223, "New post2",
+            Reposts(0, false),
+            Views(0),
+            "string",
+            postSource = null,
+            null,
+            null,
+            copyHistory = ArrayList<Reposts>(),
+            null,
+            null,
+            null,
+            true,
+            true,
+            true,
+            Donut(true, 10, placeHolder = null, true, "string"),
+            100,
+            attachments = null
+        )
+        val testPost2 = Post(
+            0,
+            111,
+            222,
+            111,
+            1679447042,
+            "New post2",
+            333,
+            444,
+            true,
+            null,
+            Copyright(5, "www", "String", "String"),
             Likes(0, 0, true, true),
-            Comments(0, canPost = true, groupsCanPost = true, canClose = true, canOpen = true))
+            Reposts(0, false),
+            Views(0),
+            "string",
+            postSource = null,
+            null,
+            null,
+            copyHistory = ArrayList<Reposts>(),
+            null,
+            null,
+            null,
+            true,
+            true,
+            true,
+            Donut(true, 10, placeHolder = null, true, "string"),
+            100,
+            attachments = null
+        )
         add(post = testPost1)
         add(post = testPost2)
-        val testPostUpdated = Post(3, 111_000, 222_000, "New post Updated",
+        val testPostUpdated = Post(
+            3,
+            111,
+            222,
+            111,
+            1679447042,
+            "New post updated",
+            333,
+            444,
+            true,
+            null,
+            Copyright(5, "www", "String", "String"),
             Likes(0, 0, true, true),
-            Comments(0, canPost = true, groupsCanPost = true, canClose = true, canOpen = true))
+            Reposts(0, false),
+            Views(0),
+            "string",
+            postSource = null,
+            null,
+            null,
+            copyHistory = ArrayList<Reposts>(),
+            null,
+            null,
+            null,
+            true,
+            true,
+            true,
+            Donut(true, 10, placeHolder = null, true, "string"),
+            100,
+            attachments = null
+        )
         val result = update(post = testPostUpdated)
 
-        assertEquals(false, result)
+        assertFalse("false", result)
 
     }
 }
